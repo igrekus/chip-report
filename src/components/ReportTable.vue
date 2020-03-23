dd53c9f7d47f91d77a59532c362eccff7cd2f73c
igrekus <upload234@mail.ru>
2020-03-23 16:55:10 +0300

    FIX: empty input tag warning

--- src/components/ReportTable.vue
+++ src/components/ReportTable.vue
@@ -10,7 +10,7 @@
       <template v-slot:top>
         <q-item>
           <q-input v-model.number="table.rows" type="number" dense label="Количество строк"
-                   @input="onRowNumChanged(table.id)"/>&nbsp;&nbsp;&nbsp;
+                   @input="onRowNumChanged(table.id)"></q-input>
         </q-item>
         <q-item>
           <q-input v-model="table.header" dense label="Заголовок:"></q-input>
